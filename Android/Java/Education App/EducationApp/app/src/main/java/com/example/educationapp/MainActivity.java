e6cd6be00f4650804cf1328e9ebe757ec6571871
Muhammad Umair <115687926+Umair786786@users.noreply.github.com>
2023-11-14 04:12:04 -0800

    Added Education App

---
+++ Android/Java/Education App/EducationApp/app/src/main/java/com/example/educationapp/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.educationapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+};
+    }
