3c8ec9073d508c96456422d75fbf2504a8d75fe4
Muhammad Hasnain <134514900+Hasnain3815@users.noreply.github.com>
2023-11-14 16:55:01 +0500

    Added Tareen Sports Academy

---
+++ Android/Java/TareenSportsAcademy/app/src/main/java/com/example/tareensportsacademy/MainActivity2.java
@@ -0,0 +1,35 @@
+package com.example.tareensportsacademy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+
+        Button button = findViewById(R.id.reg);
+
+ button.setOnClickListener(new View.OnClickListener(){
+ @Override
+public void onClick(View view)
+ {
+
+
+
+Intent intent = new Intent(MainActivity2.this, MainActivity3.class);
+
+ startActivity(intent);
+
+ finish();
+};
+    });
+}
+}
