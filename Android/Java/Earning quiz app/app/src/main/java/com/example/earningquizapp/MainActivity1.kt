dcea287c70751483ddc05b4052bda0f05804a07e
talhabinjameel <talhabinjameel@gmail.com>
2023-11-14 00:06:41 -0800

    added earning app

---
+++ Android/Java/Earning quiz app/app/src/main/java/com/example/earningquizapp/MainActivity1.kt
@@ -0,0 +1,11 @@
+package com.example.earningquizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.mainactivity_1)
+    }
+}
