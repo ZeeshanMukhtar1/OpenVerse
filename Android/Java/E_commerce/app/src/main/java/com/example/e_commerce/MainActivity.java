a448d015bf4361c908c88fca912bc4d1d304dff7
kashifshahmuhammad <135092807+kashifshahmuhammad@users.noreply.github.com>
2023-11-15 11:24:24 +0500

    E commerce online store

---
+++ Android/Java/E_commerce/app/src/main/java/com/example/e_commerce/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.e_commerce;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn = findViewById(R.id.nextBtn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+          }
+}
