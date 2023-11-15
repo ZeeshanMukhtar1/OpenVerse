af7a824c1c50293581489de4ac0386da82e48631
Akifmalik5699 <97229500+Akifmalik5699@users.noreply.github.com>
2023-11-15 10:14:03 +0500

    added akif and aqsa project

---
+++ Android/Kotlin/OpenTables/app/src/main/java/com/example/opentables/ActivityStart.kt
@@ -0,0 +1,11 @@
+package com.example.opentables
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityStart : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+    }
+}
---
+++ Android/Kotlin/OpenTables/app/src/main/java/com/example/opentables/SplashScreen.kt
@@ -0,0 +1,19 @@
+package com.example.opentables
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+@Suppress("DEPRECATION")
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        Handler().postDelayed({
+            val intent = Intent(this, StartActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
